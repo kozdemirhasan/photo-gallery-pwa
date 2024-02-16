e102f16af938df1c4fbcf0c661330c6b2e3f73ee
kozdemirhasan <kozdemirhasan@gmail.com>
2024-02-16 17:31:44 +0100

    update

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+// var express = require("express");
+// var app = express();
+// var path = require("path");
 
-app.use(express.static(path.join(__dirname)));
+// app.use(express.static(path.join(__dirname)));
+
+// app.get("/", function (req, res) {
+//     res
+// 		.status(200)
+// 		.sendFile(path.join(__dirname, "/index.html"));
+// });
+// app.listen(3000, function () {
+//     console.log("The server is running at localhost:3000");
+// });
+
+
+
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    res
-		.status(200)
-		.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-app.listen(3000, function () {
-    console.log("The server is running at localhost:3000");
+
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
 });
